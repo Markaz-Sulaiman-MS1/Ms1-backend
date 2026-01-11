08a98d618b996de2f04da7b43298ee3cb411d351
althafkl711 <113635353+althafkl711@users.noreply.github.com>
2026-01-11 17:04:11 +0530

    Fix the logo issue

--- MsOne/settings.py
+++ MsOne/settings.py
@@ -155,6 +155,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 AWS_S3_FILE_OVERWRITE = False
